e8518b03a875b4bd734f29e5ac52bcec8c487bdc
Alperen ARSLAN <105153770+ARSAlperen@users.noreply.github.com>
2022-06-06 08:02:06 +0300

    Update Assignment2.py

--- Assignment2.py
+++ Assignment2.py
@@ -3,7 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
 data=pd.read_csv(r"C:\Users\anka0\Desktop\Data Science with Python\visulalization\assignement.csv")
-# data["Date"]=pd.to_datetime(data["Date"],unit="D")
 data=data.sort_values(by=['Date',"ID","Element"]).reset_index(drop=True)
 
 s=0
@@ -76,16 +75,18 @@ plt.figure()
 # observation_dates = list(map(pd.to_datetime, observation_dates)) # convert the map to a list to get rid of the error
 plt.plot(max_date,max_temp,'blue',min_date,min_temp,'red',markersize=1,linestyle='-')
 
+plt.scatter(scatter_max_2015_date,scatter_max_2015,alpha=1,c='black')
+plt.scatter(scatter_min_2015_date,scatter_min_2015,alpha=1,c='magenta')
 # fill the area between the linear data and exponential data
 plt.gca().fill_between(range(len(max_date)), #lenght
                        max_temp, min_temp, #data interval to fill
-                       facecolor='yellow', 
-                       alpha=0.80) #transparency
-plt.legend(['Maximum Temperatures', 'Minimum Temperatures'])
+                       facecolor='grey', 
+                       alpha=0.30) #transparency
 
-plt.scatter(scatter_max_2015_date,scatter_max_2015,alpha=1,c='black')
-plt.scatter(scatter_min_2015_date,scatter_min_2015,alpha=1,c='black')
 
+
+
+plt.legend(['Maximum Temperatures', 'Minimum Temperatures','High temperature records broken in 2015','Low temperature records broken in 2015'])
 ax = plt.gca()
 ax.set_xlabel('Days of Year')
 ax.set_ylabel('Temperatures')
@@ -93,6 +94,6 @@ ax.set_title("Highest and Lowest Temperatures between 2005-2014 in Ann Arbor, Mi
 ax.set_xticks([])
 plt.tick_params(top=False, bottom=False, left=True, right=False)
 
-
+plt.set_figheight(50)
+plt.set_figwidth(50)
 plt.show()
-
